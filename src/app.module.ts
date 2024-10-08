5118d39ed5caaddda86f57a100b9687a0475a8f4
QING的台式机\10554 <1055428094@qq.com>
2024-10-09 05:30:13 +0800

    初始化模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingModule } from './booking/booking.module';
+import { StatisticModule } from './statistic/statistic.module';
+import { RolesModule } from './roles/roles.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./config/.env.development'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          host: configService.get('MYSQL_HOST'),
+          port: configService.get('MYSQL_PORT'),
+          username: configService.get('MYSQL_USERNAME'),
+          password: configService.get('MYSQL_PASSWORD'),
+          connectorPackage: 'mysql2',
+          database: 'meeting',
+          synchronize: true,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          type: 'mysql',
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
+    RoomsModule,
+    BookingModule,
+    StatisticModule,
+    RolesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8011);
+  await app.listen(1024);
 }
 bootstrap();
